10d8581e75297c28e7db9e27cd43cc1caa25f1a6
Kai Hoffman <notkaiho@gmail.com>
2019-01-09 18:03:13 +0000

    My pair is a hero

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,20 @@
 require './lib/bike'
 
 class DockingStation
+  attr_reader :docked_bikes
+
+def initialize(docked_bikes = Array.new)
+  @docked_bikes = docked_bikes
+end
+
   def release_bike
-    Bike.new
+    fail "No bikes!" if @docked_bikes.count == 0
+    @docked_bikes
   end
+
+  def dock (bike)
+    fail "I'm full!" if @docked_bikes.count >= 20
+    @docked_bikes.push(bike)
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,8 +5,8 @@ describe Bike do
   it { is_expected.to respond_to :working? }
 end
 
+context 'Bike can self-identify as working'
 describe Bike.new.working? do
-  context 'Bike can self-identify as working'
   #described_class.new.working?
     it { is_expected.to be true }
 end
